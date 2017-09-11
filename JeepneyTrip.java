cb37f29097f0da278ec5d87a78387fb9c87194fc
julie aureo <julie.aureo@gmail.com>
2017-09-11 20:44:45 +0800

    Update JeepneyTrip.java

--- JeepneyTrip.java
+++ JeepneyTrip.java
@@ -1,3 +1,25 @@
+/* signature
+ * double --> double
+ * purpose
+ * produces the change given the amount, the number of passengers and discounts 
+ * 
+ * stub
+ * double fareChange(){
+ * returns 0.0;
+ * }
+ * 
+ * examples
+ * 
+ * template
+ * ...this.distance -- double
+ * ...this.totalPassengers -- int
+ * ...this.discountPassengers -- int
+ * 
+ * body/method
+ * 
+ * test and debug
+ */
+
 class JeepneyTrip {
   double distance;
   int totalPassengers;
@@ -5,6 +27,8 @@ class JeepneyTrip {
   
   // constants
   final int BASE_KM = 5;
+  final double BASE_FARE = 7.00;
+  final double BASE_DISCOUNT = 6.00;
   
   
   JeepneyTrip(double distance, int totalPassengers, int discountPassengers) {
@@ -14,6 +38,30 @@ class JeepneyTrip {
   }
   
   double fareChange(double payment) {
-    return 0;
+    if(this.distance <= 5){
+      return payment - (totalPassengerFare() + totalDiscountPassenger());
+    }   
+    else {
+      return payment - (((totalDistanceFare() + BASE_FARE)*this.totalPassengerFare())+((totalDistanceFare() + BASE_DISCOUNT)*this.discountPassengers));
+    }
+  }
+   
+  int normal(){
+    return this.totalPassengers - this.discountPassengers;
+  }
+  
+  double totalDistanceFare(){
+    double reduced = this.distance - BASE_KM;
+    double distanceFare = reduced * 0.50;
+    return distanceFare;
+  }
+  double totalPassengerFare(){
+    double totalNormalFare = normal() * BASE_FARE;
+    return totalNormalFare;
+  }
+  double totalDiscountPassenger(){
+    double totalDiscountFare = this.discountPassengers*BASE_DISCOUNT;
+    return totalDiscountFare;
   }
 }
+  
